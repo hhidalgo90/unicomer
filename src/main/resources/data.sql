6200dcebf2ed8b1f9c5422a92687c9369ea7fd0c
Hector Hidalgo <hhidalgo@kdu.cl>
2022-05-31 20:40:03 -0400

    Se agrega profiles y bd H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO prospecto(id, last_name, address_home, birth_day, cell_phone, first_name, gender, home_phone, incomes, profession) VALUES
+(1,'Hidalgo Espinoza','Alto manquehue 3194','1990-11-26','962234900','Hector','1','2943233','10000USD','Software engineer');
+
+INSERT INTO prospecto(id, last_name, address_home, birth_day, cell_phone, first_name, gender, home_phone, incomes, profession) VALUES
+(2,'Maldonado Merino','El tamarugo 857','1994-08-12','956624785','Andres','1','2958733','5000USD','Senior Software engineer');
+
+INSERT INTO prospecto(id, last_name, address_home, birth_day, cell_phone, first_name, gender, home_phone, incomes, profession) VALUES
+(3,'Espinoza Salinas','Los castanos 3194','2000-03-02','962234900','Andrea','0','3558745','1000USD','QA');
